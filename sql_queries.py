19351fa0efedc34cd7d5d88a87753d9aa2f8daab
Yacine Benzerga <benzerga.yacine@hotmail.com>
2019-12-09 14:04:09 -0500

    feat(init queries): added create&drop table queries

--- sql_queries.py
+++ sql_queries.py
@@ -13,7 +13,7 @@ songplay_table_create = ("""
         user_agent VARCHAR
     )""")
 
-users_table_create = ("""CREATE TABLE IF NOT EXISTS users (
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
     user_id INT PRIMARY KEY,
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
@@ -46,3 +46,17 @@ time_table_create = ("""
         weekday NUMERIC NOT NULL
     )
 """)
+
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+
+create_tbl_queries = [time_table_create, user_table_create,
+                      artist_table_create, song_table_create, songplay_table_create]
+drop_tbl_queries = [songplay_table_drop, user_table_drop,
+                    song_table_drop, artist_table_drop, time_table_drop]
